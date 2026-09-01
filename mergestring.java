78170e46283bc973257dd076cdfc7d0bd6038da8
MonishaAnandan <monishaanand697@gmail.com>
2026-09-01 11:59:37 +0530

    Add files via upload

---
+++ mergestring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner ref=new Scanner(System.in);
+	    String n1=ref.nextLine();
+	    String n2=ref.nextLine();
+		System.out.println(n1+n2);
+	}
+}
